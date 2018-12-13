182511a0d58bb9189667c3a1e8dcd3837127d723
pimpina <pimpinan_bum@truecorp.co.th>
2018-12-13 15:36:18 +0700

    Create StructDevice.go

---
+++ tvsstructs/StructDevice.go
@@ -0,0 +1,13 @@
+package tvsstructs
+
+// Device Obj
+type Device struct {
+	DeviceID              int
+	ModelID               int
+	SerialNumber          string
+	StatusID              int
+	StockhandlerID        int
+	StockReceiveDetailsID int
+	FromStockhandlerID    int
+	ShipOrderID           int
+}
