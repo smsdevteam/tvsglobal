ca402b833343a96c560a99212b8683532290a0aa
Nattachaison <45254347+Nattachaison@users.noreply.github.com>
2018-12-13 15:07:40 +0700

    changename

---
+++ tvsstructs/note.go
@@ -0,0 +1,17 @@
+package tvsstructs
+
+import (
+ 
+	"time"
+)
+
+type Note struct {
+	CustomerID        int32
+	CreatedByUserID   int32
+	ActionUserID      int32
+	CategoryID        string
+	CompletionStageID string
+	Body              string
+	NoteID            int32
+	CreateDateTime    time.Time
+}
