88bef571b9d42c45dfb2c59742657db510542818
noppadolda <kunoodd@hotmail.com>
2018-12-20 11:12:15 +0700

    ood

--- struct/contact.go
+++ struct/contact.go
@@ -1,4 +1,4 @@
-package struct
+package structwww
 
 import (
 	"fmt"
---
+++ struct/tvs_order_req.go
@@ -0,0 +1,10 @@
+package struct_tvs_order_req
+
+import (
+	"fmt"
+	"time"
+)
+
+type Tvs_order_req struct {
+	 order_id string
+}
