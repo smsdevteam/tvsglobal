708a2977a1aee82f8c1f85687fe4fcc722a4dfd7
noppadolda <kunoodd@hotmail.com>
2019-01-31 13:55:00 +0700

    Update tvsnote_main.go

--- TVSNote/tvsnote_main.go
+++ TVSNote/tvsnote_main.go
@@ -50,7 +50,7 @@ func getListNote(w http.ResponseWriter, r *http.Request) {
 
 func createNote(w http.ResponseWriter, r *http.Request) {
 
-	body, err := ioutil.ReadAll(r.Body)
+	body, err := ioutil.ReadAll(r.Body) 
 	if err != nil {
 		panic(err)
 	}
