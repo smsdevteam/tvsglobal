b4dbfda49f9854ce4ab06547a55de321be717f72
armbodyslam <arm.arnon@hotmail.com>
2019-02-14 16:25:43 +0700

    update keyword
    2019-02-14

---
+++ TVSKeyword/tvskeyword_main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	st "github.com/smsdevteam/tvsglobal/tvsstructs"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to TVS Keyword Restful")
+}
+
+func main() {
+	fmt.Println("Service Start...")
+	mainRouter := mux.NewRouter().StrictSlash(true)
+	mainRouter.HandleFunc("/tvskeyword", index)
+	mainRouter.HandleFunc("/tvskeyword/getkeyword/{keywordid}", getKeyword)
+	log.Fatal(http.ListenAndServe(":8000", mainRouter))
+}
+
+func getKeyword(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	//fmt.Println(params["noteid"])
+
+	var keywordResult *st.GetKeywordResult
+
+	keywordResult = GetKeywordByKeywordID(params["keywordid"])
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(keywordResult)
+}
--- TVSStructs/keyword.go
+++ TVSStructs/keyword.go
@@ -1,11 +1,36 @@
 package tvsstructs
 
+import (
+	"time"
+)
+
+//Keyword obj
 type Keyword struct {
-	Attribute     string `json:"Attribute"`
-	Description   string `json:"Description"`
-	Hide          int32  `json:"Hide"`
-	ID            int64  `json:"Id"`
-	Keyword       string `json:"Keyword"`
-	KeywordTypeID int64  `json:"KeywordTypeId"`
-	Name          string `json:"Name"`
+	AllowedKeywordID int64     `json:"allowed_keyword_id"`
+	Attribute        string    `json:"attribute"`
+	CountValue       int64     `json:"count_value"`
+	CustomerID       int64     `json:"customer_id"`
+	DateValue        time.Time `json:"date_value"`
+	ID               int64     `json:"id"`
+	KAAttribute      string    `json:"kaattribute"`
+	KAKeyword        string    `json:"kakeyword"`
+	KALongDescr      string    `json:"kalongdescr"`
+	KeyTypesID       int64     `json:"keytypes_id"`
+	KTName           string    `json:"ktname"`
+	KTUserKey        string    `json:"ktuserkey"`
+}
+
+//GetKeywordResult obj
+type GetKeywordResult struct {
+	MyKeyword Keyword `json:"keyword"`
+	ErrorCode int     `json:"errorcode"`
+	ErrorDesc string  `json:"errordesc"`
+}
+
+// NewGetKeywordResult Obj
+func NewGetKeywordResult() *GetKeywordResult {
+	return &GetKeywordResult{
+		ErrorCode: 1,
+		ErrorDesc: "Unexpected Error",
+	}
 }
