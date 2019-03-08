e7d87ba4208b1c39d4c9b8c6ee5cd7997aacf920
noppadolda <kunoodd@hotmail.com>
2019-03-08 11:05:47 +0700

    commit;

--- TVSQUEUEPROCESS/tvs_processflow.go
+++ TVSQUEUEPROCESS/tvs_processflow.go
@@ -17,7 +17,55 @@ func generatetasklist(Trackingno string, TVSOrdReq  st.TVSSubmitOrderToQueue) (s
 
 	var TVSOrdRes st.TVSQueueSubmitOrderReponse
 	var resultI driver.Rows
+	var err error
 	cm.ExcutestoreDS("ICC", `begin tvs_servorder.generatetasklist(:p_trackingno,:p_ordertype,:p_rs );  end;`,
 		Trackingno, TVSOrdReq.TVSOrdReq.OrderType, sql.Out{Dest: &resultI})
-	return "", TVSOrdRes
+	 	defer resultI.Close()
+		values := make([]driver.Value, len(resultI.Columns()))
+		colmap := cm.Createmapcol(resultI.Columns())
+		for {
+			print(colmap)
+			err = resultI.Next(values)
+			if err == nil {
+				if err == io.EOF {
+					break
+				}
+			} else {
+				break
+			}
+			TVSBNP.TRNSEQNO = values[cm.Getcolindex(colmap, "TRNSEQNO")].(string) // values[colmap["TRNSEQNO"]].(string)
+			TVSBNP.CCBSorderno = values[colmap["CCBS_ORDERNO"]].(string)
+			//custno, err := strconv.Atoi(values[colmap["TVS_CUSTOMERNO"]].(string)) // strconv.ParseInt( values[2].(string), 10, 64)
+			//if err != nil {
+			//	TVSBNP.TVSCUSTOMERNO = custno
+			//	}
+	
+			TVSBNP.TVSAccountno = values[colmap["TVS_ACCOUNTNO"]].(string)
+			TVSBNP.SHNO = values[colmap["SHNO"]].(string)
+			TVSBNP.CCBSFN = values[colmap["CCBSFN"]].(string)
+			TVSBNP.RECALLOFFER = values[colmap["RECALLOFFER"]].(string)
+			TVSBNP.HAVEOCCHARE = values[colmap["HAVEOCCHARE"]].(string)
+			TVSBNP.OMXOrderType = values[colmap["OMXORDERTYPE"]].(string)
+			TVSBNP.TVSCustomerType = values[colmap["TVS_CUSTOMERTYPE"]].(string)
+			TVSBNP.CCBSACTIVITYREON = values[colmap["CCBS_ACTIVITYREASON"]].(string)
+			TVSBNP.CCBSUSERTEXT = values[colmap["CCBS_USERTEXT"]].(string)
+			TVSBNP.CCBSCustomerno = values[colmap["CCBS_CUSTOMERNO"]].(string)
+			TVSBNP.CCBSAccountno = values[colmap["CCBS_ACCOUNTNO"]].(string)
+			TVSBNP.CCBSOUNo = values[colmap["CCBS_OUNO"]].(string)
+			TVSBNP.CCBSSubNo = values[colmap["CCBS_SUBNO"]].(string)
+			TVSBNP.CCBSURLSERVICE = values[colmap["CCBSURL_SERVICE"]].(string)
+			TVSBNP.Refvalue = values[colmap["REFVALUE"]].(string)
+			TVSBNP.Reftype = values[colmap["REFTYPE"]].(string)
+			TVSBNP.ToCCBSCustomerno = values[colmap["TO_CCBS_CUSTOMERNO"]].(string)
+			TVSBNP.OLDCCBSACCOUNTNO = values[colmap["OLD_CCBSACCOUNTNO"]].(string)
+			TVSBNP.Oldccbssubno = values[colmap["OLD_CCBSSUBNO"]].(string)
+			TVSBNP.AddSOCLevelOU = values[colmap["ADDSOC_LEVEL_OU"]].(string)
+			TVSBNP.TVSBNOMXPropertyobj.Channel = values[colmap["CHANNEL"]].(string)
+			TVSBNP.TVSBNOMXPropertyobj.DealerCode = values[colmap["DEALERCODE"]].(string)
+			TVSBNP.TVSBNOMXPropertyobj.EffectiveDateSpecified = cm.StrToInt(values[colmap["EFFECTIVEDATESPECIFIED"]].(string))
+			TVSBNP.TVSBNOMXPropertyobj.EffectiveDate = values[colmap["EFFECTIVEDATE"]].(string)
+	
+			TVSBNP.TVSBNCCBSOfferPropertylist = getccbsoffer(TVSBNP)
+		}
+		return "", TVSOrdRes
 }
--- TVSStructs/tvsqueueorder.go
+++ TVSStructs/tvsqueueorder.go
@@ -2,6 +2,7 @@ package tvsstructs
 
 import "time"
 
+// TVSQueuSubmitOrderRequest object  call from client
 type TVSQueuSubmitOrderRequest struct {
 	Orderid     string
 	OrderType   string
@@ -10,14 +11,25 @@ type TVSQueuSubmitOrderRequest struct {
 	TVSCustNo   int64
 	Custinfo    CustomerInfo
 }
+
+// TVSSubmitOrderToQueue object for send to rabbit mq
 type TVSSubmitOrderToQueue struct {
-	Trackingno        string
-	TVSOrdReq TVSQueuSubmitOrderRequest
-	
+	Trackingno  string
+	TVSOrdReq   TVSQueuSubmitOrderRequest
+	TVSTaskList []TVSTaskinfo
 }
 
+// TVSQueueSubmitOrderReponse object for response to client
 type TVSQueueSubmitOrderReponse struct {
 	Orderid           string
 	Trackingno        string
 	ResponseResultobj ResponseResult
+}
+
+// TVSTaskinfo object for task process info
+type TVSTaskinfo struct {
+	Taskid    string
+	Taskname  string
+	MSname    string
+	Resultobj ResponseResult
 }
