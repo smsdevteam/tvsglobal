3d88b547b67b8d85905305ab1e415587bee3b4b4
noppadolda <kunoodd@hotmail.com>
2019-03-11 11:34:02 +0700

    commit

--- TVSBN/tvsbnservice_main.go
+++ TVSBN/tvsbnservice_main.go
@@ -4,14 +4,14 @@ import (
 	//"encoding/json"
 	"encoding/json"
 	"fmt"
-
+	"io/ioutil"
 	"log"
 	"net/http"
 
 	cm "github.com/smsdevteam/tvsglobal/common"
 
 	"github.com/gorilla/mux"
-	//st "tvsglobal/tvsstructs"
+	st "github.com/smsdevteam/tvsglobal/TVSStructs"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
@@ -25,11 +25,33 @@ func main() {
 	//restoresub(108218909)
 	mainRouter := mux.NewRouter().StrictSlash(true)
 	mainRouter.HandleFunc("/tgovsbn/ccbschangepackage/{customerid}", ccbschangepackage)
+	mainRouter.HandleFunc("/tgovsbn/ccbschangepackage2", ccbschangepackage2).Methods("POST")
 	mainRouter.HandleFunc("/tgovsbn/ccbssuspendsub/{customerid}", ccbssuspendsub)
 	mainRouter.HandleFunc("/tgovsbn/ccbsrestoresub/{customerid}", ccbsrestoresub)
 
 	log.Fatal(http.ListenAndServe(":8000", mainRouter))
 }
+func ccbschangepackage2(w http.ResponseWriter, r *http.Request) {
+	temp, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+	//Read Json Request
+	var req st.CustomerInfo
+	err = json.Unmarshal(temp, &req)
+	if err != nil {
+		fmt.Println("There was an error:", err)
+		panic(err)
+	}
+	var oRes string
+
+	oRes = changepackage(cm.StrToInt(req.ID))
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(oRes)
+
+}
+
 func ccbschangepackage(w http.ResponseWriter, r *http.Request) {
 	//var customerid int64
 	//var err error
