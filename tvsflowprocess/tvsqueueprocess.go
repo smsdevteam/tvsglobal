7e695c063bdeeea4a85399831d00869967d9b9c7
noppadolda <kunoodd@hotmail.com>
2019-03-12 16:32:02 +0700

    pood

--- tvsflowprocess/tvsqueueprocess.go
+++ tvsflowprocess/tvsqueueprocess.go
@@ -95,3 +95,24 @@ func initialtask(tvssubmitdata st.TVSSubmitOrderData) {
 
 	}
 }
+func callserv() {
+    url := "http://restapi3.apiary.io/notes"
+    fmt.Println("URL:>", url)
+
+    var jsonStr = []byte(`{"title":"Buy cheese and bread for breakfast."}`)
+    req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+    req.Header.Set("X-Custom-Header", "myvalue")
+    req.Header.Set("Content-Type", "application/json")
+
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close()
+
+    fmt.Println("response Status:", resp.Status)
+    fmt.Println("response Headers:", resp.Header)
+    body, _ := ioutil.ReadAll(resp.Body)
+    fmt.Println("response Body:", string(body))
+}
